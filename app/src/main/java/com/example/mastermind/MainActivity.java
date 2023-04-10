e9260389e31ca05765818e855cf1fbd7eae5d53c
Nia <88581938+NiaD95@users.noreply.github.com>
2023-04-10 23:04:31 +0200

    correccion errores

--- app/src/main/java/com/example/mastermind/MainActivity.java
+++ app/src/main/java/com/example/mastermind/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.mastermind;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import androidx.core.content.ContextCompat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -38,27 +39,21 @@ public class MainActivity extends AppCompatActivity {
         //asignamos las listView
         asignartListViews();
 
-        /*//agregamos info a listas
-        cargarInfoListViews();*/
-
-        //agrega los botones a una lista
-        // agregarAlistasBotones();
-
     }
     private void asignarBotonesColores() {
         bComenzar = findViewById(R.id.bComenzar);
 
         //botones de abajo
-        bAzul = (Button) findViewById(R.id.bAzul);
-        bAmarillo = (Button) findViewById(R.id.bAmarillo);
-        bRojo = (Button) findViewById(R.id.bRojo);
-        bVerde = (Button) findViewById(R.id.bVerde);
+        bAzul = findViewById(R.id.bAzul);
+        bAmarillo = findViewById(R.id.bAmarillo);
+        bRojo = findViewById(R.id.bRojo);
+        bVerde = findViewById(R.id.bVerde);
 
         //botones de arriba
-        bAdivinar1 = (Button) findViewById(R.id.bAdivinar1);
-        bAdivinar2 = (Button) findViewById(R.id.bAdivinar2);
-        bAdivinar3 = (Button) findViewById(R.id.bAdivinar3);
-        bAdivinar4 = (Button) findViewById(R.id.bAdivinar4);
+        bAdivinar1 = findViewById(R.id.bAdivinar1);
+        bAdivinar2 = findViewById(R.id.bAdivinar2);
+        bAdivinar3 = findViewById(R.id.bAdivinar3);
+        bAdivinar4 = findViewById(R.id.bAdivinar4);
     }
     private void asignartListViews() {
         lvAciertos = findViewById(R.id.lv2);
@@ -73,89 +68,61 @@ public class MainActivity extends AppCompatActivity {
         reiniciar();
 
 
-        while (intentos==10 && !listaRandomColors.equals(listaIntentos)){
+        while (intentos<10 && !listaRandomColors.equals(listaIntentos)){
             // limpiar la selección de botones de colores
-           // botonesSeleccionados.clear();
+            botonesSeleccionados.clear();
 
             Toast.makeText(this,"Has perdido, Intentalo de nuevo",Toast.LENGTH_SHORT).show();
 
             //espera a que se presione los botones de colores
-           while (botonesSeleccionados.size() < listaRandomColors.size()){
-               vecesPuladasYComparaListas();
-               intentos++;
-           }
-        }
-        if (listaRandomColors.equals(lvIntentos)){
-            Toast.makeText(this,"¡Felicidades!, pulsa de nuevo 'comenza' para iniciar otra partida",Toast.LENGTH_SHORT).show();
-        }
-    }
-    private void vecesPuladasYComparaListas() {
-        Map<Integer,Integer> contadorCliksBotonesMaps = new HashMap<>(); //almacenamos los contadores de cata boton en esta variable
 
-        for (final Button boton : botonesColores){
-
-            //agregamos contador para cada boton del mapa
-            contadorCliksBotonesMaps.put(boton.getId(),0);
-
-            //definimos el listener para el boton
-            boton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int contadorBoton = contadorCliksBotonesMaps.get(boton.getId());
-
-                    contadorCliksBotonesMaps.put(boton.getId(),contadorBoton+1); //contabilizamos el contador del mapa
-
-                    //generamos una lista de botones selecionados
-                    List<Boton> listaBotonesSelect = (List<Boton>) lvIntentos.getAdapter();
-                    //comparamos posición y color de ñps botones aleatorios
-                    for ( int i=0; i<listaBotonesSelect.size(); i++ ){
-                        int pos = listaBotonesSelect.get(i).getPosicion();
-                        int color = listaBotonesSelect.get(i).getColor();
-
-
-                        List<Boton> listaBotonesRandom = listaRandomColors;
-                        //generamos lista de botones random
-                        for (int t=0; t<listaBotonesRandom.size(); t++){
-                            int posRnd= listaBotonesRandom.get(t).getPosicion();
-                            int colorRnd= listaBotonesRandom.get(t).getColor();
-
-                            //comparamos las listas y  mostramoss tipos de aciertos
-
-                            int menorIgual4= 1;
-                            int mayor4=2;
-                            switch (contadorCliksBotonesMaps.size()<=botonesColores.length ? menorIgual4:mayor4){ //por repasar bien
-                                case 1:
-                                    if (pos == posRnd && color == colorRnd) {
-                                        bAdivinar1.setBackgroundColor(getColor(R.color.negro));
-                                        bAdivinar2.setBackgroundColor(getColor(R.color.negro));
-                                        bAdivinar3.setBackgroundColor(getColor(R.color.negro));
-                                        bAdivinar4.setBackgroundColor(getColor(R.color.negro));
-                                        acabarPartida();
-                                    } else if (pos != posRnd && color != colorRnd) { //caso absoluto de nunguno coincide
-                                        bAdivinar1.setBackgroundColor(getColor(R.color.blanco));
-                                        bAdivinar2.setBackgroundColor(getColor(R.color.blanco));
-                                        bAdivinar3.setBackgroundColor(getColor(R.color.blanco));
-                                        bAdivinar4.setBackgroundColor(getColor(R.color.blanco));
-                                    } else if (pos != posRnd && color == colorRnd) { // coincide color pero no la posición
-                                        bAdivinar1.setBackgroundColor(getColor(R.color.gris));
-                                        bAdivinar2.setBackgroundColor(getColor(R.color.gris));
-                                        bAdivinar3.setBackgroundColor(getColor(R.color.gris));
-                                        bAdivinar4.setBackgroundColor(getColor(R.color.gris));
-                                     }
-                                    break;
-                                case 2:
-                                    Toast.makeText(MainActivity.this, "Te has pasado pulsando los botones, solo de hadmiten 4 pulsaciones", Toast.LENGTH_SHORT).show();
-                                    break;
-                            }
-                            cargarInfoListViews();
-                        }
+            while (botonesSeleccionados.size() < listaRandomColors.size()) {
+                vecesPulsadasYComparaListas();
+                if (botonesSeleccionados.size() == 4) { // Si se han pulsado los 4 botones, comparar listas
+                    intentos++;
+                    if (listaRandomColors.equals(listaIntentos)) { // Si las listas son iguales, el usuario ha ganado
+                        Toast.makeText(this,"¡Felicidades, has ganado!",Toast.LENGTH_SHORT).show();
+                        break;
+                    } else if (intentos == 10) { // Si se han agotado los intentos, el usuario ha perdido
+                        Toast.makeText(this,"Has perdido, intentalo de nuevo",Toast.LENGTH_SHORT).show();
+                        break;
+                    } else { // Si aún hay intentos disponibles, continuar jugando
+                        Toast.makeText(this,"Intento "+ intentos,Toast.LENGTH_SHORT).show();
+                        reiniciar();
                     }
                 }
-            });
+            }
+        }
+    }
+    private void vecesPulsadasYComparaListas() {
+        List<Boton> listaBotonesSeleccionados = new ArrayList<>(botonesSeleccionados);
+
+        if (listaBotonesSeleccionados.size() == 4) {
+            for (int i = 0; i < listaBotonesSeleccionados.size(); i++) {
+                Boton botonSeleccionado = listaBotonesSeleccionados.get(i);
+                Boton botonAdivinar = listaRandomColors.get(i);
+
+                if (botonSeleccionado.getColor() == botonAdivinar.getColor() && botonSeleccionado.getPosicion() == botonAdivinar.getPosicion()) {
+                    botonSeleccionado.setColor(R.color.negro);
+                } else if (botonSeleccionado.getColor() == botonAdivinar.getColor() && botonSeleccionado.getPosicion() != botonAdivinar.getPosicion()) {
+                    botonSeleccionado.setColor(R.color.gris);
+                } else {
+                    botonSeleccionado.setColor(R.color.blanco);
+                }
+            }
+
+            cargarInfoListViews();
+
+            if (listaBotonesSeleccionados.equals(listaRandomColors)) {
+                Toast.makeText(this, "Has ganado", Toast.LENGTH_SHORT).show();
+                acabarPartida();
+            } else if (intentos == 10 ){
+                Toast.makeText(this, "Has perdido, Intentalo de nuevo", Toast.LENGTH_SHORT).show();
+                acabarPartida();
+            }
         }
     }
     private void cargarInfoListViews() {
-
         //agregamos los elemento a las listas usando sus adaptadores personalizados
 
         botonesElegidosAdapter = new BotonAdapter(this, botonesSeleccionados);
@@ -163,8 +130,6 @@ public class MainActivity extends AppCompatActivity {
 
         lvIntentos.setAdapter(botonesElegidosAdapter);
         lvAciertos.setAdapter(botonesAciertosAdapter);
-
-
     }
     private void acabarPartida() {
         //mostrar las combinación random en los botonesde arriba
@@ -177,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
 
             botonesAdivinar[pos].setBackgroundColor(getColor(color));
         }
+
     }
     private void borrarContenidoListView() {
         botonesAciertosAdapter.clear();
